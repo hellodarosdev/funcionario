6cfcff7ab6642302d38cbaaab2ebe51770d42234
Heloise Daros <hsd178br@gmail.com>
2026-05-07 15:56:06 -0300

    implementa request e response

--- src/main/java/br/com/gestao/funcionarios/application/api/FuncionarioRequest.java
+++ src/main/java/br/com/gestao/funcionarios/application/api/FuncionarioRequest.java
@@ -1,5 +1,23 @@
 package br.com.gestao.funcionarios.application.api;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+import java.math.BigDecimal;
+import java.util.UUID;
 
+@Value
 public class FuncionarioRequest {
-
+    private UUID id;
+    @NotBlank
+    private String nome;
+    @NotBlank
+    private String designacao;
+    @NotBlank
+    private BigDecimal salario;
+    @NotBlank
+    private String telefone;
+    @NotBlank
+    private String endereco;
+    @NotNull
+    private Boolean aceitaTermos;
 }
--- src/main/java/br/com/gestao/funcionarios/application/api/FuncionarioResponse.java
+++ src/main/java/br/com/gestao/funcionarios/application/api/FuncionarioResponse.java
@@ -1,4 +1,22 @@
 package br.com.gestao.funcionarios.application.api;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 public class FuncionarioResponse {
+    private UUID id;
+    @NotBlank
+    private String nome;
+    @NotBlank
+    private String designacao;
+    @NotBlank
+    private BigDecimal salario;
+    @NotBlank
+    private String telefone;
+    @NotBlank
+    private String endereco;
+    @NotNull
+    private Boolean aceitaTermos;
 }
